8c5799dc79d57729f1feaaf786d2ea00d0224846
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-05-22 20:11:54 +0530

    Added Swagger Support

--- src/main/java/com/librarysystem/LibraryManagementAppApplication.java
+++ src/main/java/com/librarysystem/LibraryManagementAppApplication.java
@@ -2,12 +2,29 @@ package com.librarysystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
- public class LibraryManagementAppApplication {
+@EnableSwagger2
+public class LibraryManagementAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementAppApplication.class, args);
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.basePackage("com.librarysystem.controller"))              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
 
 }
--- src/main/java/com/librarysystem/controller/BookController.java
+++ src/main/java/com/librarysystem/controller/BookController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
