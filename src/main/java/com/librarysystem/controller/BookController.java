dffc0036ecf4fcb63dd435f4f6be5c2860f50cb5
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-05-22 14:59:26 +0530

    New Book Controller

--- src/main/java/com/librarysystem/controller/BookController.java
+++ src/main/java/com/librarysystem/controller/BookController.java
@@ -1,10 +1,9 @@
 package com.librarysystem.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class LibraryController {
-	
-	
+public class BookController {
 
 }
