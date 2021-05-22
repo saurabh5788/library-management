86212091ab3f2abf9d221abe51e08ccb7da5d0ae
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-05-22 15:51:00 +0530

    User ID added as Path Var

--- src/main/java/com/librarysystem/controller/BookController.java
+++ src/main/java/com/librarysystem/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class BookController {
 
 	@GetMapping("/{userid}/books")
 	public ResponseEntity<?> fetchBooksByUser(
+			@PathVariable("userid") String userId,
 			@RequestParam Optional<String> authorName,
 			@RequestParam Optional<String> title,
 			@RequestParam Optional<String> category,
