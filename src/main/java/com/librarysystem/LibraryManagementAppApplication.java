bf48e0060a5c08b07aa7a6f8d55e36898466aadd
gsaikumar016 <gsaikumar016@gmail.com>
2021-05-22 13:43:23 +0530

    initial

---
+++ src/main/java/com/librarysystem/LibraryManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.librarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+ public class LibraryManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementAppApplication.class, args);
+	}
+
+}
