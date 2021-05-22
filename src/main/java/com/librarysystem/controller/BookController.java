399b73449bcf764ba5f59eeae63ff8ea718fc61f
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-05-22 15:16:42 +0530

    Initial Book Controller

--- src/main/java/com/librarysystem/controller/BookController.java
+++ src/main/java/com/librarysystem/controller/BookController.java
@@ -1,9 +1,24 @@
 package com.librarysystem.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(MediaType.APPLICATION_JSON_VALUE)
 public class BookController {
 
-	
+	@GetMapping("/{userid}/books")
+	public ResponseEntity<?> fetchBooksByUser(
+			@RequestParam Optional<String> authorName,
+			@RequestParam Optional<String> title,
+			@RequestParam Optional<String> category,
+			@RequestParam String pageNo) {
+		return null;
+	}
 }
