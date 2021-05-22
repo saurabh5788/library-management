8af1d4854f255781f38d2842c58551f28b13ed19
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-05-22 18:53:01 +0530

    Corrected Title filter

--- src/main/java/com/librarysystem/repository/BookRepository.java
+++ src/main/java/com/librarysystem/repository/BookRepository.java
@@ -12,7 +12,7 @@ public interface BookRepository extends JpaRepository<Book, Integer> {
 	@Query("SELECT b FROM Book b "
 			+ "WHERE "
 			+ "(:author is null or b.author = :author) "
-			+ "and (:title is null or b.author = :title) "
+			+ "and (:title is null or b.title = :title) "
 			+ "and (:categoryId is null or b.category.id = :categoryId)")
 	List<Book> fetchBooksByAuthorTitleCategory(@Param("author") String author,
 			@Param("title") String title,
