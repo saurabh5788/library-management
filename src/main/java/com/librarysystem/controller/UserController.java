2b18fb4cef248ae494218bf02ac211d4e720ce2e
Saurabh Singh <saurabh.singh5788@gmail.com>
2021-05-22 15:42:34 +0530

    New User Controller Initial Add

---
+++ src/main/java/com/librarysystem/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.librarysystem.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+
+	
+}
